528b9ecd67654a9906937371756868bab782f636
Shota Iwami <saxo-folatre1129@outlook.jp>
2020-11-03 01:41:26 +0900

    [update]add hello2 func

--- sample_func.ex
+++ sample_func.ex
@@ -1,8 +1,20 @@
 defmodule SampleFunc do
   def hello(person) do
-    IO.puts "Hello, #{person}. My pid is #{inspect self()}."
+    IO.puts("Hello, #{person}. My pid is #{inspect(self())}.")
+
     receive do
-      message -> IO.puts "Message is #{message}."
+      message -> IO.puts("Message is #{message}.")
+    end
+  end
+
+  def hello2(person) do
+    IO.puts("Hello, #{person}. My pid is #{inspect(self())}.")
+
+    receive do
+      message ->
+        IO.puts("Message is #{message}.")
+        # メッセージを受信し、処理が完了したら自分自身を呼び出す
+        hello2(person)
     end
   end
 end
